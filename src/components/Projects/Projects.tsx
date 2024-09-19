4d545cb60da15acbc0dd5f88952e13d783fcf3fd
protoss70 <tlegendgaming@gmail.com>
2024-09-19 11:58:43 +0200

    Init

---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ProjectType } from "../../utility/types";
+import "./Project_Row.css";
+
+interface ProjectsProps {
+  project: ProjectType;
+}
+
+const ProjectRow: React.FC<ProjectsProps> = () => {
+
+  return (
+    <></>
+  );
+};
+
+export default ProjectRow;
