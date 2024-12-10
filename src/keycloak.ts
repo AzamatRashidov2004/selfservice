06b5f12c9cc3c51fe117a22c564a0bf529326a4e
protoss70 <tlegendgaming@gmail.com>
2024-12-10 11:38:22 +0100

    Keycloak development version

--- src/keycloak.ts
+++ src/keycloak.ts
@@ -1,14 +1,18 @@
 import Keycloak from 'keycloak-js';
 
+const environment = import.meta.env.VITE_ENVIRONMENT;
+
+export const kronosApiUrl: string = `${import.meta.env[`VITE_KRONOS_URL_${environment}`]}`;
+
 // Function to determine if the app is running on localhost
 const isLocalhost = () => {
   return window.location.hostname === 'localhost';
 };
-
+console.log("environment keycloak", `VITE_KEYCLOACK_${environment}_CLIENT_ID`)
 const keycloak = new Keycloak({
   url: import.meta.env.VITE_KEYCLOACK_URL,
   realm: import.meta.env.VITE_KEYCLOACK_REALM,
-  clientId: isLocalhost() ? import.meta.env.VITE_KEYCLOACK_CLIENT_ID : import.meta.env.VITE_KEYCLOACK_PRODUCTION_CLIENT_ID,
+  clientId: isLocalhost() ? import.meta.env.VITE_KEYCLOACK_CLIENT_ID : `${import.meta.env[`VITE_KEYCLOACK_${environment}_CLIENT_ID`]}`,
 });
 
 export default keycloak;
