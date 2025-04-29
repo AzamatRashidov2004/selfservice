b9eed26542b84526f34608ec85aaf106a289426c
Azamat Rashidov <azamatrashidov.11@gmail.com>
2025-04-29 15:44:38 +0200

    fix: testing

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 
-// https://vitejs.dev/config/ testing
+// https://vitejs.dev/config/ test
 export default defineConfig({
   plugins: [react()],
   define: {
